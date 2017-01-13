c279e0f3051539e5eefe48834fcb440a59c97685
Adam Gian <adamgian1@gmail.com>
2017-01-13 12:10:20 +1000

    Initial commit

---
+++ angular-ui-toggle.js
@@ -0,0 +1,36 @@
+/*
+* Toggle switch directive.
+*
+*
+*/
+
+angular
+	.module('uiToggle', [])
+	.directive('uiToggle', function() {
+
+		return {
+
+			replace: true,
+			restrict: 'AE',
+
+			template: function(element, attrs) {
+				var html = 	'<span ';
+					html +=		'class="ui-toggle ' + (attrs.class ? attrs.class : '') + '"';
+					html +=		attrs.ngModel
+									? ' ng-click="' + attrs.disabled + ' ? ' + attrs.ngModel + ' : ' + attrs.ngModel + '=!' + attrs.ngModel + (attrs.ngChange ? '; ' + attrs.ngChange + '()"' : '"')
+									: '';
+					html +=		'ng-class="{ checked:' + attrs.ngModel + ', disabled:' + attrs.disabled + '}"';
+					html += 	'>';
+					html +=		'<input type="checkbox" ';
+					html +=			attrs.id ? 'id="' + attrs.id + '"' : '';
+					html +=			attrs.name ? 'name="' + attrs.name + '"' : '';
+					html +=			attrs.ngModel ? 'ng-model="' + attrs.ngModel + '"' : '';
+					html +=			'style="display:none"';
+					html +=			'>';
+					html +=	'</span>';
+				return html;
+			}
+
+		};
+
+	});
