439c09ff9ea6df3abd01b17f9cbfa8844dea212a
Adam Gian <adamgian1@gmail.com>
2017-01-13 17:01:01 +1000

    Convert source js to es6 standard

--- src/angular-ui-toggle.js
+++ src/angular-ui-toggle.js
@@ -14,21 +14,17 @@ angular
             restrict: 'AE',
 
             template: function(element, attrs) {
-                var html =  '<span ';
-                    html +=     'class="ui-toggle ' + (attrs.class ? attrs.class : '') + '"';
-                    html +=     attrs.ngModel
-                                    ? ' ng-click="' + attrs.disabled + ' ? ' + attrs.ngModel + ' : ' + attrs.ngModel + '=!' + attrs.ngModel + (attrs.ngChange ? '; ' + attrs.ngChange + '()"' : '"')
-                                    : '';
-                    html +=     'ng-class="{ checked:' + attrs.ngModel + ', disabled:' + attrs.disabled + '}"';
-                    html +=     '>';
-                    html +=     '<input type="checkbox" ';
-                    html +=         attrs.id ? 'id="' + attrs.id + '"' : '';
-                    html +=         attrs.name ? 'name="' + attrs.name + '"' : '';
-                    html +=         attrs.ngModel ? 'ng-model="' + attrs.ngModel + '"' : '';
-                    html +=         'style="display:none"';
-                    html +=         '>';
-                    html += '</span>';
-                return html;
+                return `
+                    <span class="ui-toggle ${ attrs.class ? attrs.class : '' }"
+                        ${ attrs.ngModel ? ' ng-click="' + attrs.disabled + ' ? ' + attrs.ngModel + ' : ' + attrs.ngModel + '=!' + attrs.ngModel + (attrs.ngChange ? '; ' + attrs.ngChange + '()" ' : '" ') : ' ' }
+                        ng-class="{ checked: ${ attrs.ngModel }, disabled: ${ attrs.disabled } }"
+                        >
+                        <input type="checkbox"
+                            ${ attrs.id ? ' id="' + attrs.id + '"' : ' ' }
+                            ${ attrs.name ? ' name="' + attrs.name + '"' : ' ' }
+                            ${ attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : ' ' }
+                            >
+                    </span>`;
             }
 
         };
