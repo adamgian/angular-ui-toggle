efd67bda648ba2bdc52c84b80da6286e9222ca80
Adam Gian <adamgian1@gmail.com>
2017-01-17 13:42:06 +1000

    Start of refactor of directive to component

--- src/angular-ui-toggle.js
+++ src/angular-ui-toggle.js
@@ -4,29 +4,39 @@
 *
 */
 
+var uiToggle = {
+
+    bindings: {
+        class: '@?',
+        disabled: '<?',
+        id: '@?',
+        name: '<?',
+        ngChange: '&?',
+        ngModel: '=',
+    },
+
+    controller: function($scope, $attrs) {
+        var $ctrl = this;
+
+        $ctrl.toggleState = function() {
+            $ctrl.ngModel = !$ctrl.ngModel;
+            if ($ctrl.ngChange) $scope.$eval($ctrl.ngChange);
+        };
+    },
+
+    template: function($attrs) {
+        return `
+            <span class="ui-toggle"
+                ng-class="[$ctrl.class, { 'is-active': $ctrl.ngModel }]"
+                ng-click="$ctrl.toggleState()"
+                >
+                <input
+                    >
+            </span>`;
+    }
+
+};
+
 angular
     .module('uiToggle', [])
-    .directive('uiToggle', function() {
-
-        return {
-
-            replace: true,
-            restrict: 'AE',
-
-            template: function(element, attrs) {
-                return `
-                    <span class="ui-toggle ${ attrs.class ? attrs.class : '' }"
-                        ${ attrs.ngModel ? ' ng-click="' + attrs.disabled + ' ? ' + attrs.ngModel + ' : ' + attrs.ngModel + '=!' + attrs.ngModel + (attrs.ngChange ? '; ' + attrs.ngChange + '()" ' : '" ') : ' ' }
-                        ng-class="{ checked: ${ attrs.ngModel }, disabled: ${ attrs.disabled } }"
-                        >
-                        <input type="checkbox"
-                            ${ attrs.id ? ' id="' + attrs.id + '"' : ' ' }
-                            ${ attrs.name ? ' name="' + attrs.name + '"' : ' ' }
-                            ${ attrs.ngModel ? ' ng-model="' + attrs.ngModel + '"' : ' ' }
-                            >
-                    </span>`;
-            }
-
-        };
-
-    });
+    .component('uiToggle', uiToggle);
