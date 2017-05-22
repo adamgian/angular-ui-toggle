351a323381996f8d81e4022b80b12c9b7f9282d0
Matt Carter <m@ttcarter.com>
2017-05-22 13:31:48 +1000

    REFACTOR: Rewrote as Angular Component

--- dist/angular-ui-toggle.js
+++ dist/angular-ui-toggle.js
@@ -1,35 +1,20 @@
 'use strict';
 
-/*
-* Toggle switch directive.
-*
-*
-*/
-
-var uiToggle = {
-
+angular.module('uiToggle', []).component('uiToggle', {
 	bindings: {
 		class: '@?',
 		disabled: '<?',
-		id: '@?',
-		name: '<?',
 		ngChange: '&?',
-		ngDisabled: '=?',
+		ngDisabled: '<',
 		ngModel: '='
 	},
-
-	controller: function controller($scope, $attrs) {
+	controller: ["$scope", "$attrs", function controller($scope, $attrs) {
 		var $ctrl = this;
+		$ctrl.$attrs = $attrs || {};
 
 		$ctrl.toggleState = function () {
-			if ($ctrl.ngChange) $scope.$eval($ctrl.ngChange);
+			if ($ctrl.ngChange) $ctrl.ngChange({ value: $ctrl.ngModel });
 		};
-	},
-
-	template: function template($attrs) {
-		return '\n\t\t\t<span class="ui-toggle"\n\t\t\t\tng-class="$ctrl.class"\n\t\t\t\tng-click="$ctrl.toggleState()"\n\t\t\t\t>\n\t\t\t\t<input type="checkbox"\n\t\t\t\t\t' + ('id' in $attrs && 'id="' + $attrs.id + '"') + '\n\t\t\t\t\t' + ('name' in $attrs && 'name="' + $attrs.name + '"') + '\n\t\t\t\t\tng-model="$ctrl.ngModel"\n\t\t\t\t\tng-disabled="$ctrl.ngDisabled || ' + ('disabled' in $attrs) + '"\n\t\t\t\t\t>\n\t\t\t\t<div class="ui-toggle__track"></div>\n\t\t\t\t<div class="ui-toggle__thumb"></div>\n\t\t\t</span>';
-	}
-
-};
-
-angular.module('uiToggle', []).component('uiToggle', uiToggle);
+	}],
+	template: '\n\t\t\t<span class="ui-toggle" ng-class="$ctrl.class" ng-click="$ctrl.toggleState()">\n\t\t\t\t<input type="checkbox"\n\t\t\t\t\tng-model="$ctrl.ngModel"\n\t\t\t\t\tng-disabled="$ctrl.ngDisabled || $ctrl.$attrs.disabled !== undefined"\n\t\t\t\t/>\n\t\t\t\t<div class="ui-toggle__track"></div>\n\t\t\t\t<div class="ui-toggle__thumb"></div>\n\t\t\t</span>\n\t\t'
+});
--- src/angular-ui-toggle.js
+++ src/angular-ui-toggle.js
@@ -1,48 +1,29 @@
-/*
-* Toggle switch directive.
-*
-*
-*/
-
-var uiToggle = {
-
-	bindings: {
-		class: '@?',
-		disabled: '<?',
-		id: '@?',
-		name: '<?',
-		ngChange: '&?',
-		ngDisabled: '=?',
-		ngModel: '='
-	},
-
-	controller: function($scope, $attrs) {
-		var $ctrl = this;
-
-		$ctrl.toggleState = function() {
-			if ($ctrl.ngChange) $scope.$eval($ctrl.ngChange);
-		};
-	},
-
-	template: function($attrs) {
-		return `
-			<span class="ui-toggle"
-				ng-class="$ctrl.class"
-				ng-click="$ctrl.toggleState()"
-				>
-				<input type="checkbox"
-					${ 'id' in $attrs && 'id="' + $attrs.id + '"' }
-					${ 'name' in $attrs && 'name="' + $attrs.name + '"' }
-					ng-model="$ctrl.ngModel"
-					ng-disabled="$ctrl.ngDisabled || ${ 'disabled' in $attrs }"
-					>
-				<div class="ui-toggle__track"></div>
-				<div class="ui-toggle__thumb"></div>
-			</span>`;
-	}
-
-};
-
 angular
 	.module('uiToggle', [])
-	.component('uiToggle', uiToggle);
+	.component('uiToggle', {
+		bindings: {
+			class: '@?',
+			disabled: '<?',
+			ngChange: '&?',
+			ngDisabled: '<',
+			ngModel: '=',
+		},
+		controller: function($scope, $attrs) {
+			var $ctrl = this;
+			$ctrl.$attrs = $attrs || {};
+
+			$ctrl.toggleState = ()=> {
+				if ($ctrl.ngChange) $ctrl.ngChange({value: $ctrl.ngModel});
+			};
+		},
+		template: `
+			<span class="ui-toggle" ng-class="$ctrl.class" ng-click="$ctrl.toggleState()">
+				<input type="checkbox"
+					ng-model="$ctrl.ngModel"
+					ng-disabled="$ctrl.ngDisabled || $ctrl.$attrs.disabled !== undefined"
+				/>
+				<div class="ui-toggle__track"></div>
+				<div class="ui-toggle__thumb"></div>
+			</span>
+		`,
+	});
