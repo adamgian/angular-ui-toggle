8540fec7ef4d05f731d37a146ff96728a1f97405
Matt Carter <m@ttcarter.com>
2017-05-22 12:57:43 +1000

    REFACTOR: Whitespace 2/4 spaces -> tab

--- dist/angular-ui-toggle.js
+++ dist/angular-ui-toggle.js
@@ -8,27 +8,27 @@
 
 var uiToggle = {
 
-    bindings: {
-        class: '@?',
-        disabled: '<?',
-        id: '@?',
-        name: '<?',
-        ngChange: '&?',
-        ngDisabled: '=?',
-        ngModel: '='
-    },
+	bindings: {
+		class: '@?',
+		disabled: '<?',
+		id: '@?',
+		name: '<?',
+		ngChange: '&?',
+		ngDisabled: '=?',
+		ngModel: '='
+	},
 
-    controller: function controller($scope, $attrs) {
-        var $ctrl = this;
+	controller: function controller($scope, $attrs) {
+		var $ctrl = this;
 
-        $ctrl.toggleState = function () {
-            if ($ctrl.ngChange) $scope.$eval($ctrl.ngChange);
-        };
-    },
+		$ctrl.toggleState = function () {
+			if ($ctrl.ngChange) $scope.$eval($ctrl.ngChange);
+		};
+	},
 
-    template: function template($attrs) {
-        return '\n            <span class="ui-toggle"\n                ng-class="$ctrl.class"\n                ng-click="$ctrl.toggleState()"\n                >\n                <input type="checkbox"\n                    ' + ('id' in $attrs && 'id="' + $attrs.id + '"') + '\n                    ' + ('name' in $attrs && 'name="' + $attrs.name + '"') + '\n                    ng-model="$ctrl.ngModel"\n                    ng-disabled="$ctrl.ngDisabled || ' + ('disabled' in $attrs) + '"\n                    >\n                <div class="ui-toggle__track"></div>\n                <div class="ui-toggle__thumb"></div>\n            </span>';
-    }
+	template: function template($attrs) {
+		return '\n\t\t\t<span class="ui-toggle"\n\t\t\t\tng-class="$ctrl.class"\n\t\t\t\tng-click="$ctrl.toggleState()"\n\t\t\t\t>\n\t\t\t\t<input type="checkbox"\n\t\t\t\t\t' + ('id' in $attrs && 'id="' + $attrs.id + '"') + '\n\t\t\t\t\t' + ('name' in $attrs && 'name="' + $attrs.name + '"') + '\n\t\t\t\t\tng-model="$ctrl.ngModel"\n\t\t\t\t\tng-disabled="$ctrl.ngDisabled || ' + ('disabled' in $attrs) + '"\n\t\t\t\t\t>\n\t\t\t\t<div class="ui-toggle__track"></div>\n\t\t\t\t<div class="ui-toggle__thumb"></div>\n\t\t\t</span>';
+	}
 
 };
 
--- src/angular-ui-toggle.js
+++ src/angular-ui-toggle.js
@@ -6,43 +6,43 @@
 
 var uiToggle = {
 
-    bindings: {
-        class: '@?',
-        disabled: '<?',
-        id: '@?',
-        name: '<?',
-        ngChange: '&?',
-        ngDisabled: '=?',
-        ngModel: '='
-    },
+	bindings: {
+		class: '@?',
+		disabled: '<?',
+		id: '@?',
+		name: '<?',
+		ngChange: '&?',
+		ngDisabled: '=?',
+		ngModel: '='
+	},
 
-    controller: function($scope, $attrs) {
-        var $ctrl = this;
+	controller: function($scope, $attrs) {
+		var $ctrl = this;
 
-        $ctrl.toggleState = function() {
-            if ($ctrl.ngChange) $scope.$eval($ctrl.ngChange);
-        };
-    },
+		$ctrl.toggleState = function() {
+			if ($ctrl.ngChange) $scope.$eval($ctrl.ngChange);
+		};
+	},
 
-    template: function($attrs) {
-        return `
-            <span class="ui-toggle"
-                ng-class="$ctrl.class"
-                ng-click="$ctrl.toggleState()"
-                >
-                <input type="checkbox"
-                    ${ 'id' in $attrs && 'id="' + $attrs.id + '"' }
-                    ${ 'name' in $attrs && 'name="' + $attrs.name + '"' }
-                    ng-model="$ctrl.ngModel"
-                    ng-disabled="$ctrl.ngDisabled || ${ 'disabled' in $attrs }"
-                    >
-                <div class="ui-toggle__track"></div>
-                <div class="ui-toggle__thumb"></div>
-            </span>`;
-    }
+	template: function($attrs) {
+		return `
+			<span class="ui-toggle"
+				ng-class="$ctrl.class"
+				ng-click="$ctrl.toggleState()"
+				>
+				<input type="checkbox"
+					${ 'id' in $attrs && 'id="' + $attrs.id + '"' }
+					${ 'name' in $attrs && 'name="' + $attrs.name + '"' }
+					ng-model="$ctrl.ngModel"
+					ng-disabled="$ctrl.ngDisabled || ${ 'disabled' in $attrs }"
+					>
+				<div class="ui-toggle__track"></div>
+				<div class="ui-toggle__thumb"></div>
+			</span>`;
+	}
 
 };
 
 angular
-    .module('uiToggle', [])
-    .component('uiToggle', uiToggle);
+	.module('uiToggle', [])
+	.component('uiToggle', uiToggle);
