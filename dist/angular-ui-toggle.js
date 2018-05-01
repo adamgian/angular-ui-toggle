0763e75dd26e3c80201a411521a2aacd2e6c47ae
Matt Carter <m@ttcarter.com>
2018-05-01 10:53:18 +1000

    Version bump

--- dist/angular-ui-toggle.js
+++ dist/angular-ui-toggle.js
@@ -11,38 +11,36 @@
  * @param {function} [ngChange] Function to call as ({value}) on value change
  */
 angular.module('uiToggle', []).component('uiToggle', {
-  bindings: {
-    class: '@?',
-    disabled: '<?',
-    ngDisabled: '<?'
-  },
-  require: {
-    ngModel: 'ngModel'
-  },
-  controller: ["$scope", "$attrs", function controller($scope, $attrs) {
+	bindings: {
+		class: '@?',
+		disabled: '<?',
+		ngDisabled: '<?'
+	},
+	require: {
+		ngModel: 'ngModel'
+	},
+	controller: ["$scope", "$attrs", function controller($scope, $attrs) {
 
-    var $ctrl = this;
+		var $ctrl = this;
 
-    $ctrl.$attrs = $attrs || {};
-    $ctrl.value = null;
+		$ctrl.$attrs = $attrs || {};
+		$ctrl.value = null;
 
-    $scope.$watch(function () {
-      if ($ctrl.ngModel) return $ctrl.ngModel.$modelValue;
-    }, function (val) {
-      if (val !== $ctrl.value) {
-        $ctrl.value = val;
-      }
-    });
+		$scope.$watch(function () {
+			return $ctrl.ngModel ? $ctrl.ngModel.$modelValue : undefined;
+		}, function (val) {
+			if (val !== $ctrl.value) $ctrl.value = val;
+		});
 
-    $scope.disabled = function () {
-      return $ctrl.ngDisabled !== undefined ? $ctrl.ngDisabled : $ctrl.$attrs.disabled !== undefined;
-    };
+		$scope.disabled = function () {
+			return $ctrl.ngDisabled !== undefined ? $ctrl.ngDisabled : $ctrl.$attrs.disabled !== undefined;
+		};
 
-    $ctrl.toggleState = function () {
-      if ($scope.disabled()) return;
-      $ctrl.value = !$ctrl.value;
-      $ctrl.ngModel.$setViewValue($ctrl.value);
-    };
-  }],
-  template: '\n    <span class="ui-toggle" ng-class="$ctrl.class" ng-click="$ctrl.toggleState()">\n    <span class="ui-toggle-input" ng-class="{checked: $ctrl.value, disabled: disabled()}"></span>\n    <div class="ui-toggle__track"></div>\n    <div class="ui-toggle__thumb"></div>\n    </span>\n    '
+		$ctrl.toggleState = function () {
+			if ($scope.disabled()) return;
+			$ctrl.value = !$ctrl.value;
+			$ctrl.ngModel.$setViewValue($ctrl.value);
+		};
+	}],
+	template: '\n\t\t\t<span class="ui-toggle" ng-class="$ctrl.class" ng-click="$ctrl.toggleState()">\n\t\t\t\t<span class="ui-toggle-input" ng-class="{checked: $ctrl.value, disabled: disabled()}"></span>\n\t\t\t\t<div class="ui-toggle__track"></div>\n\t\t\t\t<div class="ui-toggle__thumb"></div>\n\t\t\t</span>\n\t\t'
 });
